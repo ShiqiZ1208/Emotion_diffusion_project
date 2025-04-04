07c2355f78cb3fb6cfe64724baf03ed1c9e2ab58
ShiqiZ1208 <ZHANG3T3@Uwindsor.ca>
2025-04-04 12:29:39 -0600

    update all

---
+++ Emotion_detection.py
@@ -0,0 +1,78 @@
+import torch
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, set_seed, pipeline, BartForConditionalGeneration, BartTokenizer
+import nltk
+
+seed = 0
+torch.manual_seed(seed)
+set_seed(seed)
+
+def emodetect(ckpt, text):
+    emotion_model_path = ckpt
+
+    emotion_tokenizer = AutoTokenizer.from_pretrained(emotion_model_path)
+    emotion_model = AutoModelForSequenceClassification.from_pretrained(emotion_model_path)
+    emotion_classifier = pipeline("text-classification", model=emotion_model, tokenizer=emotion_tokenizer)
+
+    nltk.download('punkt')
+    nltk.download('punkt_tab')
+    go_emotions_mapping = {
+        0: 'admiration',
+        1: 'amusement',
+        2: 'anger',
+        3: 'annoyance',
+        4: 'approval',
+        5: 'caring',
+        6: 'confusion',
+        7: 'curiosity',
+        8: 'desire',
+        9: 'disappointment',
+        10: 'disapproval',
+        11: 'disgust',
+        12: 'embarrassment',
+        13: 'excitement',
+        14: 'fear',
+        15: 'gratitude',
+        16: 'grief',
+        17: 'joy',
+        18: 'love',
+        19: 'nervousness',
+        20: 'optimism',
+        21: 'pride',
+        22: 'realization',
+        23: 'relief',
+        24: 'remorse',
+        25: 'sadness',
+        26: 'surprise',
+        27: 'neutral'
+    }
+    emotion_output = emotion_classifier(text)
+    label_str = emotion_output[0]['label']
+
+    if label_str.startswith("LABEL_"):
+        label_str = label_str.replace("LABEL_", "")
+    emotion_label = int(label_str)
+
+    # Map the numeric label to its corresponding emotion name
+    emotion = go_emotions_mapping.get(emotion_label, "unknown")
+
+    return emotion
+
+def diasum(ckpt, dialogue):
+    tokenizer = tokenizer = BartTokenizer.from_pretrained('facebook/bart-large-cnn')
+    model = torch.load(ckpt, weights_only=False)
+    model.eval()
+    input_ids = tokenizer(dialogue, truncation=True, padding='max_length', max_length=512, return_tensors= "pt")
+    input_ids.to("cuda")
+    model.to("cuda")
+    output_ids = model.generate(**input_ids, max_length = 256)
+    summary = tokenizer.decode(output_ids[0], skip_special_tokens=True)
+
+    return summary
+
+def BARTsum(text):
+    summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+    summary_output = summarizer(text, max_length=130, min_length=30, do_sample=False)
+    summary = summary_output[0]['summary_text']
+
+    return summary
+
---
+++ Emotiondiffusionpipline.py
@@ -0,0 +1,58 @@
+import torch
+from diffusers import StableDiffusionPipeline
+from Emotion_detection import emodetect, diasum, BARTsum
+from color_transfer import lab_color_transfer
+import matplotlib.pyplot as plt
+import numpy as np
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+def sdpipline(content, sumckpt, emockpt, mode = "plain"):
+  #with open(file_path, 'r') as file:
+    #content = file.read()
+
+  if mode.lower() == "plain":
+      summary = BARTsum(content)
+  elif mode.lower() == "dialogue":
+      summary = diasum(sumckpt, content)
+
+  emotion = emodetect(emockpt, summary)
+  print(f"summary:\n{summary}\nEmotion: {emotion}")
+
+  prompt = f"A emotion based painting that vividly captures the essence of '{summary}' with a mood of '{emotion}'."
+
+  backcolor = "A background color express emotion of {emotion}"
+
+  sd_model_id = "runwayml/stable-diffusion-v1-5"
+  pipe = StableDiffusionPipeline.from_pretrained(sd_model_id, torch_dtype=torch.float16)
+  pipe = pipe.to(device)
+
+  image = pipe(prompt, num_inference_steps=50, guidance_scale=7.5).images[0]
+  color = pipe(backcolor, num_inference_steps=50, guidance_scale=7.5).images[0]
+
+  np_content_image = np.array(image)
+  np_color_image = np.array(color)
+
+  alpha = 0.5
+  final_image = lab_color_transfer(np_content_image, np_color_image, alpha)
+
+  plt.figure(figsize=(8, 8))
+  plt.imshow(final_image)
+  plt.axis("off")
+  plt.title("Final Generated Image")
+  plt.show()
+
+  plt.figure(figsize=(8, 8))
+  plt.imshow(np_content_image)
+  plt.axis("off")
+  plt.title("content image without color transfer")
+  plt.show()
+
+  plt.figure(figsize=(8, 8))
+  plt.imshow(np_color_image)
+  plt.axis("off")
+  plt.title("backgorund color")
+  plt.show()
+
+  return final_image, np_content_image, np_color_image
+
